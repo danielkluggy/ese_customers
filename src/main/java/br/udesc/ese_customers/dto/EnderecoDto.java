a00292224bc301ae6391241af58ed4abb909bbd3
Daniel Larion Klug <dklug22@gmail.com>
2024-09-30 21:45:25 -0300

    Ajuste Lombok

--- src/main/java/br/udesc/ese_customers/dto/EnderecoDto.java
+++ src/main/java/br/udesc/ese_customers/dto/EnderecoDto.java
@@ -5,7 +5,9 @@ import java.util.UUID;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.Data;
 
+@Data
 public class EnderecoDto {	
 	
 	@NotBlank
@@ -35,69 +37,4 @@ public class EnderecoDto {
 	private String cep;
 	
 	private UUID clienteId;
-	
-	public String getLogradouro() {
-		return logradouro;
-	}
-
-	public void setLogradouro(String logradouro) {
-		this.logradouro = logradouro;
-	}
-
-	public Integer getNumero() {
-		return numero;
-	}
-
-	public void setNumero(Integer numero) {
-		this.numero = numero;
-	}
-
-	public String getComplemento() {
-		return complemento;
-	}
-
-	public void setComplemento(String complemento) {
-		this.complemento = complemento;
-	}
-
-	public String getBairro() {
-		return bairro;
-	}
-
-	public void setBairro(String bairro) {
-		this.bairro = bairro;
-	}
-
-	public String getCidade() {
-		return cidade;
-	}
-
-	public void setCidade(String cidade) {
-		this.cidade = cidade;
-	}
-
-	public String getEstado() {
-		return estado;
-	}
-
-	public void setEstado(String estado) {
-		this.estado = estado;
-	}
-
-	public String getCep() {
-		return cep;
-	}
-
-	public void setCep(String cep) {
-		this.cep = cep;
-	}
-
-	public UUID getClienteId() {
-		return clienteId;
-	}
-
-	public void setClienteId(UUID clienteId) {
-		this.clienteId = clienteId;
-	}
-
 }
